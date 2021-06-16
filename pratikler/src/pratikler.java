e14dae1e5c538f318ce01ed9e2b52753423b7b05
salika <salikalper@gmail.com>
2021-06-16 18:25:15 +0300

    last pratic added

--- pratikler/src/pratikler.java
+++ pratikler/src/pratikler.java
@@ -197,7 +197,107 @@ public class pratikler {
         }
 
 
+        System.out.println();
+        System.out.println("---------------------------------------------------------------------");
+        //----------------------------------------------------------------------------
+        // Altıncı pratik
+        System.out.println("------------------------------Altıncı pratik---------------------------------------");
+        System.out.println();
 
+        int month,day;
+        System.out.println("Enter your birthday");
+        day= scan.nextInt();
+
+        System.out.println("Enter your birthmonth");
+        month= scan.nextInt();
+
+
+
+        if (month==1){
+            if (day<22){
+                System.out.println("Oğlak");
+            } else{
+                System.out.println("Kova");
+            }
+
+        }else if (month==2){
+            if (day<20){
+                System.out.println("Kova");
+            } else{
+                System.out.println("Balık");
+            }
+
+        }else if (month==3){
+            if (day<21){
+                System.out.println("Balık");
+            } else{
+                System.out.println("Koç");
+            }
+
+        }else if (month==4){
+            if (day<21){
+                System.out.println("Koç");
+            } else{
+                System.out.println("Boğa");
+            }
+
+        }else if (month==5){
+            if (day<20){
+                System.out.println("Boğa");
+            } else{
+                System.out.println("İkizler");
+            }
+
+        }else if (month==6){
+            if (day<20){
+                System.out.println("İkizler");
+            } else{
+                System.out.println("yengeç");
+            }
+
+        }else if (month==7){
+            if (day<20){
+                System.out.println("yengeç");
+            } else{
+                System.out.println("Aslan");
+            }
+
+        }else if (month==8){
+            if (day<20){
+                System.out.println("Aslan");
+            } else{
+                System.out.println("Başak");
+            }
+
+        }else if (month==9){
+            if (day<20){
+                System.out.println("Başak");
+            } else{
+                System.out.println("Terazi");
+            }
+
+        }else if (month==10){
+            if (day<20){
+                System.out.println("Terazi");
+            } else{
+                System.out.println("Akrep");
+            }
+
+        }else if (month==11){
+            if (day<20){
+                System.out.println("Akrep");
+            } else{
+                System.out.println("Yay");
+            }
+
+        }else if (month==12){
+            if (day<20){
+                System.out.println("Yay");
+            } else{
+                System.out.println("Oğlak");
+            }
+
+        }
 
 
 
